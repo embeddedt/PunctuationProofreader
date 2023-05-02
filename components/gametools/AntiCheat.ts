b5569e72790131d77fb3c97928561449ff612824
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-02 11:33:41 -0400

    Anticheat patches

--- components/gametools/AntiCheat.ts
+++ components/gametools/AntiCheat.ts
@@ -5,7 +5,8 @@ let inter;
 export function startCheck() {
     let cheated = () => {
         document.open();
-        document.write('<h1>Are you trying to cheat?</h1><p><i>Those seeing this page may be cheaters.<br/>Don\'t be a cheater.</i></p><p>If you didn\'t mean to cheat, your device was probably running slowly and triggered the anti-cheat mechanism. Refresh the page and try again.</p><p>P.S. F12, Ctrl+U, and Ctrl+Shift+I are all ways to open Developer Tools - an easy way to cheat (except in this game).</p>');
+        document.write('<p>This game will not let you open the developer tools. Reload your page and try again.</p>');
+        //<h1>Are you trying to cheat?</h1><p><i>Those seeing this page may be cheaters.<br/>Don\'t be a cheater.</i></p><p>If you didn\'t mean to cheat, your device was probably running slowly and triggered the anti-cheat mechanism. Refresh the page and try again.</p><p>P.S. F12, Ctrl+U, and Ctrl+Shift+I are all ways to open Developer Tools - an easy way to cheat (except in this game).</p>');
         document.close();
         stopCheck();
         setTimeout(cheated, 2000);
@@ -13,16 +14,18 @@ export function startCheck() {
     stopCheck();
 
     if(process.env.NODE_ENV == 'production') {
+        /*
         inter = setInterval(function() {
-            var minimalUserResponseInMiliseconds = 500;
+            var minimalUserResponseInMiliseconds = 10000;
             var before = new Date().getTime();
             const dbg = new Function("debugger;");
             dbg();
             var after = new Date().getTime();
-            if (after - before > minimalUserResponseInMiliseconds) { // user had to resume the script manually via opened dev tools 
+            if (after - before > minimalUserResponseInMiliseconds) { // user had to resume the script manually via opened dev tools
                 cheated();
             }
         }, 1000);
+        */
         (window as any).shortcut.add("F12", cheated);
         (window as any).shortcut.add("Ctrl+F12", cheated);
         (window as any).shortcut.add("Shift+F12", cheated);
@@ -33,8 +36,8 @@ export function startCheck() {
         (window as any).shortcut.add("Ctrl+U", cheated);
         (window as any).shortcut.add("Ctrl+Shift+I", cheated);
     }
-    
-      
+
+
 }
 
 export function stopCheck() {
